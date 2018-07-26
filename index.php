538f798364c76bb8b4a88dd4bf768c2b27272d37
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-26 14:00:07 +0200

    exo3 partie 3 php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 3</title>
+  </head>
+  <body>
+<p>
+<?php
+  $a = 100;
+  $b = 50;
+  while($a > 10){
+    echo $a * $b . '<br />';
+    $a --;
+  }
+?>
+</p>
+  </body>
+</html>
